54b9a60b2eefe408e1e838034045dd90517e1473
Stanley Huang <huang.stanley050@gmail.com>
2019-10-16 20:15:17 +1100

    made on and trigger method for User class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "steve", age: 22 });
-user.set({ name: "bob" });
-console.log(user);
+user.on("change", () => {
+  console.log("change");
+});
+user.on("change", () => {
+  console.log("change");
+});
+user.trigger("change");
+
+//console.log(user);
