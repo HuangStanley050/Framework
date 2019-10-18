33f101fcbb40f07fd7eae6864003ffb9f286731a
Stanley Huang <huang.stanley050@gmail.com>
2019-10-18 20:20:57 +1100

    adding Attributes generic class

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,22 +1,25 @@
 import axios, { AxiosPromise } from "axios";
-import { UserProps } from "./User";
 
-export class Sync {
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): void {
+  save(data: T): AxiosPromise {
     const { id } = data;
     //axios.post(``).then((res:AxiosResponse)=>{})
     if (id) {
       //put
-      axios.put(`${this.rootUrl}/${id}`, data);
+      return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
       //post
-      axios.post(this.rootUrl, data);
+      return axios.post(this.rootUrl, data);
     }
   }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,13 @@
 import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
 export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
-
+const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    this.data = { ...this.data, ...update };
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 }
