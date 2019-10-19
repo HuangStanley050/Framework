ccc005700196e7c4c57fa5bdc47b8e97620fa8f0
Stanley Huang <huang.stanley050@gmail.com>
2019-10-19 20:02:46 +1100

    adding getter

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ import { User } from "./models/User";
 const user = new User({ name: "Megaman", age: 22 });
 
 //console.log(user);
-user.events.on("change", () => console.log("change"));
-user.events.on("change", () => console.log("change again"));
-user.events.trigger("change");
+user.on("change", () => console.log("user chaning"));
