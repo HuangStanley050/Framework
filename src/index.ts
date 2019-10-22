7d1372809d0ec1414e29d8a34d9ad50986ad744d
Stanley Huang <huang.stanley050@gmail.com>
2019-10-22 19:33:00 +1100

    completed userform

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { User } from "./models/User";
-const collection = User.buildUserCollection();
-collection.on("change", () => console.log(collection));
-collection.fetch();
+import { UserForm } from "./views/UserForm";
+
+const userForm = new UserForm(document.getElementById("root"));
+
+userForm.render();
