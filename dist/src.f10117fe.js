e520c0b8fc4d011366220ee2d4d99ca78ebe9e3d
Stanley Huang <huang.stanley050@gmail.com>
2019-10-24 19:59:30 +1100

    UserShow class done

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -133,6 +133,10 @@ function () {
     this.bindModel();
   }
 
+  View.prototype.eventsMap = function () {
+    return {};
+  };
+
   View.prototype.bindModel = function () {
     var _this = this;
 
@@ -230,18 +234,23 @@ function (_super) {
       }
     };
 
+    _this.onSaveClick = function () {
+      _this.model.save();
+    };
+
     return _this;
   }
 
   UserForm.prototype.eventsMap = function () {
     return {
       "click:.set-age": this.onSetAgeClick,
-      "click:.set-name": this.onSetNameClick
+      "click:.set-name": this.onSetNameClick,
+      "click:.save-model": this.onSaveClick
     };
   };
 
   UserForm.prototype.template = function () {
-    return "\n     <div>\n      <h1>User Form</h1>\n        <div>User Name: " + this.model.get("name") + "</div>\n        <div>User Age: " + this.model.get("age") + "</div>\n        <input/>\n        <button class=\"set-name\">Update Name</button>\n        <button class='set-age'>Set Random age</button>\n     </div>\n    ";
+    return "\n     <div>\n        <input placeholder=\"" + this.model.get("name") + "\" />\n        <button class=\"set-name\">Update Name</button>\n        <button class='set-age'>Set Random age</button>\n        <button class='save-model'>Save User</button>\n     </div>\n    ";
   };
 
   return UserForm;
@@ -2325,7 +2334,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44307" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45069" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
